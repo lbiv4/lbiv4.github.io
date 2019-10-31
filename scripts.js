c95a818629d612c56c9f1b352b27562b3886093e
lbiv4 <lanebartoniv@gmail.com>
2019-10-31 12:57:41 -0700

    Added shrinking for images as screen changes size

--- scripts.js
+++ scripts.js
@@ -13,24 +13,37 @@ document.addEventListener("DOMContentLoaded", () => {
   //Functionality for changing pages
   let pageLinks = document.getElementsByClassName("navPageLink");
   let subLinks = document.getElementsByClassName("navSubLink");
-  console.log(pageLinks);
   for (let i = 0; i < pageLinks.length; i++) {
     pageLinks.item(i).addEventListener("click", () => {
       changeActive("a.active", pageLinks.item(i));
     });
   }
-  console.log("done");
 
   //Functionality for changing subLists
   window.addEventListener("scroll", () => {
-    console.log(subLinks);
     for (let i = 0; i < subLinks.length; i++) {
       let divId = subLinks.item(i).attributes["href"].value.split("#")[1];
       let subLink = document.getElementById("#" + divId);
       let linkTop = subLink.getBoundingClientRect().top;
-      console.log(linkTop);
       if (linkTop >= 0 && linkTop < 100) {
         changeActive("a.navSubLink.active", subLinks.item(i));
+      }
+    }
+  });
+
+  //Functionality for resizing images when screen shrinks. Not accounting for vertical shrink at this time
+  window.addEventListener("resize", () => {
+    const sidebarWidth = document.getElementById("sidebarDiv").offsetWidth;
+    const midWidth = (window.innerWidth - sidebarWidth) / 2;
+    const images = document.querySelectorAll(".photoRight, .photoLeft");
+    for (let i = 0; i < images.length; i++) {
+      if (images[i].naturalWidth > midWidth) {
+        const multipler = midWidth / images[i].naturalWidth;
+        images[i].width = multipler * images[i].naturalWidth;
+        images[i].height = multipler * images[i].naturalHeight;
+      } else {
+        images[i].width = images[i].naturalWidth;
+        images[i].height = images[i].naturalHeight;
       }
     }
   });
